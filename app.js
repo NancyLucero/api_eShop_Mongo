58617a0ab54aadd9563cedec47695a16034af73f
Nancy Lucero <nancylucero23@gmail.com>
2023-06-30 23:40:56 -0300

    se sube a mi rama

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 // invocamos a express
-const express = require('express');
-const app= express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // middleware
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
-
+app.use(morgan("tiny"));
 
 // Invocamos a dotenv
-require('dotenv/config');
+require("dotenv/config");
 const api = process.env.API_URL;
 
 // rutas
 
-const productoRouter = require('./routers/productos');
-const userRouter = require('./routers/users');
-const carritoRouter = require('./routers/carritos');
+const productoRouter = require("./routers/productos");
+const userRouter = require("./routers/users");
+const carritoRouter = require("./routers/carritos");
 
 /*
 const productoRouter = require('./routers/shop');*/
 
-
-app.use('/productos',productoRouter);
-app.use('/users',userRouter);
+app.use("/productos", productoRouter);
+app.use("/users", userRouter);
 
 // conectar a la BD
-mongoose.connect(process.env.CONNECTION_STRING,{
-    useNewUrlParser:true,
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'lamps'
-})
-.then(()=>{
-    console.log('BD conectada')
-})
-.catch((err)=>{
+    dbName: "lamps",
+  })
+  .then(() => {
+    console.log("BD conectada");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 // iniciar servidor
 
-app.listen(3000, ()=>{
-    //console.log(api);
-    console.log('Servidor ejecutandose')
-})
+app.listen(3000, () => {
+  //console.log(api);
+  console.log("Servidor ejecutandose");
+});
